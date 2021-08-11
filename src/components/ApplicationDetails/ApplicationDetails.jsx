770040f9350de7bb809a7ad4d283a69fa78c7564
Steve Savstrom <steve.savstrom@gmail.com>
2021-08-11 18:21:01 -0500

    formatted and commented ApplicationDetails and UserPage

--- src/components/ApplicationDetails/ApplicationDetails.jsx
+++ src/components/ApplicationDetails/ApplicationDetails.jsx
@@ -1,12 +1,13 @@
+// ApplicationDetails shows all applications for a specific job
+// ApplicationDetails allows a user to review, confirm, and reject applicants for a specific job
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import "./ApplicationDetails.css";
 
-// Material-UI
+// Material-UI imports
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import LinkedCameraOutlinedIcon from '@material-ui/icons/LinkedCameraOutlined';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -22,11 +23,14 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
+// end Material-UI imports
 
+// Dialog alert transition
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="right" ref={ref} {...props} />;
 });
 
+// Green avatar badge for confirmed applicant
 const StyledBadge = withStyles((theme) => ({
   badge: {
     backgroundColor: '#44b700',
@@ -55,7 +59,9 @@ const StyledBadge = withStyles((theme) => ({
     },
   },
 }))(Badge);
+// end Green Avatar badge
 
+// Material-UI styles
 const useStyles = makeStyles((theme) => ({
 	root: {
     justifyContent: 'center',
@@ -77,88 +83,91 @@ const useStyles = makeStyles((theme) => ({
     display: 'inline',
   },
   }));
+  // end Material-UI styles
 
 function ApplicationDetails() {
 	const applicationDetails = useSelector((store) => store.ApplicationDetailsReducer);
 	const application = useSelector((store) => store.ApplicationReducer);
-
 	const {id} = useParams();
-  console.log('is this job id?', id);
 	const dispatch = useDispatch();
 	const history = useHistory();
 	const classes = useStyles();
 
-  // const [applicationStatus, setApplicationStatus] = useState(application.status);
+  // Fetches application details on page load
+  useEffect(() => {
+		dispatch({ type: "GET_APPLICATION_DETAILS", payload: id });
+	}, []);
 
-	console.log(`***** This is applicationDetails *****`, applicationDetails);
-
+  // Back button returns user to UserPage
 	const handleBack = (event) => {
     	event.preventDefault();
     	history.push(`/user`);
   	};
 
-	useEffect(() => {
-		dispatch({ type: "GET_APPLICATION_DETAILS", payload: id });
-	}, []);
-
+  // Dispatches 'Confirmed' status on dialog click
 	const handleConfirm = (application) => {
 		console.log('*** This is handle application payload', application);
 		dispatch({ type: 'CONFIRM_APPLICATION', payload: {application, id}})
     setOpen(false);
 	}
 
+  // Dispatches 'Rejected' status on dialog click
 	const handleReject = (application) => {
 		console.log('*** This is handle reject payload', application);
 		dispatch({ type: 'REJECT_APPLICATION', payload: {application, id}})
 		window.location.reload(false);
 	}
 
-  // Dialog Alert
+  // Dialog Alert for Confirm and Reject applicants
   const [open, setOpen] = React.useState(false);
 
   // This is for the selected application to be confirmed (dialog modal)
   const [confirmId, setConfirmId] = useState('');
   const [rejectId, setRejectId] = useState('');
-    
+  
+  // Dialog alert to CONFIRM APPLICANT is opened and confirmation message appears
   const handleClickOpen = (applicationId) => {
     console.log('This is open confirm dialog by applicationId', applicationId);
     setConfirmId(applicationId)
     setOpen(true);
     };
 
+  // Dialog alert to REJECT APPLICANT is opened and confirmation message appears
   const handleRejectClickOpen = (applicationId) => {
     console.log("This is open reject dialog by applicationId", applicationId);
     setRejectId(applicationId);
     setOpen(true);
   };
-    
+  
+  // Dialog alert closes when 'No' is clicked
   const handleClose = () => {
     setOpen(false);
     };
 
-	console.log(id);
-	console.log(application);
-
 	return (
     <div className="applicationDetailsContainer">
       <div className="applicationDetailsHeader">
+        {/* Application Details Header */}
         <h1>Application Details</h1>
+        {/* Conditional message for 0 applicants */}
         {applicationDetails.length < 1 && (
           <h3>
             Sorry! You have do not have any applicants for this job yet. Check
             back later!
           </h3>
         )}
+        {/* Conditional message for 1 or more applicants */}
         {applicationDetails.length >= 1 && (
           <h3>You have {applicationDetails.length} applicants for this job</h3>
         )}
       </div>
-
+      {/* Maps through applicants and displays applicants in list on DOM */}
       {applicationDetails.map((application, index) => {
         return (
           <div className="applicationDetailsCard" key={index}>
             <List className={classes.root}>
               <ListItem alignItems="flex-start">
+              {/* Conditionally displays green avatar badge on confirmed applicants */}
               {application.status === "Confirmed" && (
                 <ListItemAvatar>
                   <StyledBadge
@@ -176,6 +185,7 @@ function ApplicationDetails() {
                   </StyledBadge>
                 </ListItemAvatar>
               )}
+              {/* Conditionally displays standard avatar badge on applied applicants */}
               {application.status === "Applied" && (
                 <ListItemAvatar>
                     <Avatar
@@ -184,6 +194,7 @@ function ApplicationDetails() {
                     />
                 </ListItemAvatar>
               )}
+              {/* Displays remaining applicant information */}
                 <ListItemText
                   primary={
                     <p>
@@ -212,6 +223,7 @@ function ApplicationDetails() {
                 />
               </ListItem>
               <div className="buttonGroup">
+                {/* Conditionally displays confirm button for applicant status: applied */}
                 {application.status === "Applied" && (
                   <Box textAlign="center" m={1}>
                     <Button
@@ -223,6 +235,7 @@ function ApplicationDetails() {
                     </Button>
                   </Box>
                 )}
+                {/* Conditionally displays reject button for applicant status: applied */}
                 {application.status === "Applied" && (
                   <Box textAlign="center" m={1}>
                     <Button
@@ -234,7 +247,7 @@ function ApplicationDetails() {
                     </Button>
                   </Box>
                 )}
-
+                {/* Conditionally displays reject button for applicant status: applied */}
                 {application.status === "Confirmed" && (
                   <Box textAlign="center" m={1}>
                     <Button
@@ -252,7 +265,9 @@ function ApplicationDetails() {
           </div>
         );
       })}
+    {/* end .map of application */}
 
+    {/* Back button returns user to UserPage */}
       <Box textAlign="center" m={3}>
         <Button
           onClick={handleBack}
@@ -280,9 +295,11 @@ function ApplicationDetails() {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
+          {/* Clicking 'NO' closes dialog alert */}
           <Button onClick={handleClose} color="primary">
             No
           </Button>
+          {/* Clicking 'YES' dispatches PUT to confirm applicant */}
           <Button
             onClick={() => handleConfirm(confirmId)}
             color="primary"
@@ -309,9 +326,11 @@ function ApplicationDetails() {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
+          {/* Clicking 'NO' closes dialog alert */}
           <Button onClick={handleClose} color="primary">
             No
           </Button>
+          {/* Clicking 'NO' dispatches PUT to reject applicant */}
           <Button
             onClick={() => handleReject(rejectId)}
             color="primary"
@@ -324,83 +343,5 @@ function ApplicationDetails() {
     </div>
   );
 }
-
-
-// This is the old card system for mapping through the applicants
-// It was replaced by the avatar list items above.
-// This is still an option if needed.
-
-{/* <div className="applicationItem">
-              <div className="buttonGroup">
-                <LinkedCameraOutlinedIcon
-                  className="icon"
-                  style={{ fontSize: 50, color: "#172536" }}
-                />
-              </div>
-              <h3>
-                New Application from {application && application.first_name}{" "}
-                {application && application.last_name}{" "}
-              </h3>
-              <p>
-                <strong>Application ID:</strong> {application && application.id}{" "}
-              </p>
-              <p>
-                <strong>Job ID:</strong> {application && application.job_id}{" "}
-              </p>
-              <p>
-                <strong>Applicant ID:</strong>{" "}
-                {application && application.applicant_id}{" "}
-              </p>
-              <p>
-                <strong>Message:</strong> {application && application.message}{" "}
-              </p>
-              <p>
-                <strong>Status:</strong> {application && application.status}{" "}
-              </p>
-              {application.status === "Confirmed" && (
-                <p>
-                  You're confirmed to work with{" "}
-                  {application && application.first_name}!{" "}
-                  <strong>Reach out by email to discuss details:</strong>{" "}
-                  {application && application.email}{" "}
-                </p>
-              )} */}
-
-            {/* Conditional rendering for confirm and reject buttons */}
-            {/* {application.status === "Applied" && (
-                <Box textAlign="center" m={1}>
-                  <Button
-                    onClick={() => handleConfirm(application)}
-                    size="small"
-                    style={{ backgroundColor: "#172536", color: "#FFFFFF" }}
-                  >
-                    Confirm
-                  </Button>
-                </Box>
-              )}
-              {application.status === "Applied" && (
-                <Box textAlign="center" m={1}>
-                  <Button
-                    onClick={() => handleReject(application)}
-                    size="small"
-                    style={{ backgroundColor: "#172536", color: "#FFFFFF" }}
-                  >
-                    Reject
-                  </Button>
-                </Box>
-              )}
-
-              {application.status === "Confirmed" && (
-                <Box textAlign="center" m={1}>
-                  <Button
-                    onClick={() => handleReject(application)}
-                    size="small"
-                    style={{ backgroundColor: "#172536", color: "#FFFFFF" }}
-                  >
-                    Reject
-                  </Button>
-                </Box>
-              )}
-            </div> */}
 
 export default ApplicationDetails;
